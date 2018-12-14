04fd3878fe80bffa2c3b8d94e98750f2aba4a2f0
Alex Kennedy <KennedyAlexJohn@gmail.com>
2018-12-14 12:13:03 +1100

    Add project files.

---
+++ TacticalGameAiTestingProject/UnitTest1.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TacticalGameAiTestingProject {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
