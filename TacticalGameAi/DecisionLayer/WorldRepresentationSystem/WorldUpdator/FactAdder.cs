fe07008488cab331d9f39a4d046df3b5bce5147c
Alex Kennedy <KennedyAlexJohn@gmail.com>
2018-12-28 16:39:27 +1100

    implemented FactAdder logic, which is a very simple delegation object

--- TacticalGameAi/DecisionLayer/WorldRepresentationSystem/WorldUpdator/FactAdder.cs
+++ TacticalGameAi/DecisionLayer/WorldRepresentationSystem/WorldUpdator/FactAdder.cs
@@ -9,15 +9,37 @@ namespace TacticalGameAi.DecisionLayer.WorldRepresentationSystem.WorldUpdator {
      * The fact adder contains a collection of 'effectadder' logic pieces which it delegates the task of applying subsesquent EFFECTS to, which occur
      * as a result of the Fact that was added. */
     class FactAdder {
-        private FactType factType;  // This is immutable. This type will be defined upon construction and is the type that is added or removed by the object.
-        private IEnumerable<IEffectAdder> effectAdders;    // This is immutable. The effect adding logic will be defined upon construction by the factory.
+        private FactType factType;
+        private IEnumerable<IEffectAdder> effectAdders;
+
+        public FactAdder(FactType factType, IEnumerable<IEffectAdder> effectAdders) {
+            this.factType = factType;
+            this.effectAdders = effectAdders;
+        }
 
         // Public Interface.
-        public void AddFact(WorldRepresentation world, int node, out Dictionary<FactType, Fact> nodeFacts) {
-
+        public void AddFact(WorldRepresentation world, int node, int value, ref Dictionary<FactType, Fact.MutableFact> nodeFacts) {
+            if (nodeFacts.ContainsKey(factType)) {
+                // Modify existing fact object's value, and that's it! The relevant effects should already exist, because the fact already exists.
+                nodeFacts[factType].SetValue(value);
+            }
+            else {
+                // New fact!
+                Fact.MutableFact f = new Fact.MutableFact(factType, value, new List<Effect>());
+                // Delegate Effect adding logic to all our IEffectAdder objects. We don't care!
+                foreach (IEffectAdder adder in effectAdders) {
+                    adder.AddEffects(world, node, f);
+                }
+            }
         }
-        public void RemoveFact(WorldRepresentation world, int node, out Dictionary<FactType, Fact> nodeFacts) {
-
+        public void RemoveFact(WorldRepresentation world, int node, ref Dictionary<FactType, Fact.MutableFact> nodeFactsToModify) {
+            if (nodeFactsToModify.ContainsKey(factType)) {
+                nodeFactsToModify.Remove(factType);
+            }
         }
+    }
+
+    interface IEffectAdder {
+        void AddEffects(WorldRepresentation world, int factNode, Fact.MutableFact factObject);
     }
 }
