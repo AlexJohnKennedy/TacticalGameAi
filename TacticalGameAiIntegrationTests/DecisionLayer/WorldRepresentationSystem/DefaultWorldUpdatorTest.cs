8a0729e36bb0fa62c35482cd253293c38e0e8685
Alex Kennedy <KennedyAlexJohn@gmail.com>
2019-01-03 18:38:17 +1100

    integration test passing on basic case for world updator and world representaiton system

--- TacticalGameAiIntegrationTests/DecisionLayer/WorldRepresentationSystem/DefaultWorldUpdatorTest.cs
+++ TacticalGameAiIntegrationTests/DecisionLayer/WorldRepresentationSystem/DefaultWorldUpdatorTest.cs
@@ -21,7 +21,65 @@ namespace TacticalGameAiIntegrationTests.DecisionLayer.WorldRepresentationSystem
             WorldRepresentation originalWorld = new WorldRepresentation(HardCodedStateCreator.CreateTestStaticState());
 
             // Ensure that everything was created correctly.
-            HardCodedStateCreator.CheckTestStaticState(originalWorld.StaticState);
+            HardCodedStateCreator.CheckTestStaticState(originalWorld.StaticState);      // Checks Static state.
+            for (int i=0; i < originalWorld.NumberOfNodes; i++) {
+                Assert.IsTrue(DynamicStateInternalReader.GetNodeFact(i, originalWorld.DynamicState).Count == 0);    // Checks that no Facts exist.
+                Assert.IsTrue(DynamicStateInternalReader.GetNodeEffectSum(i, originalWorld.DynamicState).Count == 0);   // Checks that no Effects exist.
+            }
+
+            // Mock DynamicStateChange: spotting 2 friendlies at node 0
+            Mock<IDynamicStateChange> change1 = new Mock<IDynamicStateChange>();
+            change1.Setup(c => c.AffectedNodes).Returns(new int[] { 0 });
+            Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> beforeFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 0, new KeyValuePair<FactType, int>[] { } },
+            };
+            change1.Setup(c => c.GetFactsBefore()).Returns(beforeFacts);
+            Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> afterFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 0, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.FriendlyPresence, 2)} },
+            };
+            change1.Setup(c => c.GetFactsAfter()).Returns(afterFacts);
+
+            // Mock DynamicStateChange: spotting 1 friendly at node 4 and DangerfromunknownSource at node 5
+            Mock<IDynamicStateChange> change2 = new Mock<IDynamicStateChange>();
+            change2.Setup(c => c.AffectedNodes).Returns(new int[] { 4, 5 });
+            beforeFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 4, new KeyValuePair<FactType, int>[] { } },
+                { 5, new KeyValuePair<FactType, int>[] { } },
+            };
+            change2.Setup(c => c.GetFactsBefore()).Returns(beforeFacts);
+            afterFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 4, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.FriendlyPresence, 1)} },
+                { 5, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.DangerFromUnknownSource, 5)} },
+            };
+            change2.Setup(c => c.GetFactsAfter()).Returns(afterFacts);
+
+            // Mock DynamicStateChange: spotting 3 enemies at node 2
+            Mock<IDynamicStateChange> change3 = new Mock<IDynamicStateChange>();
+            change3.Setup(c => c.AffectedNodes).Returns(new int[] { 2 });
+            beforeFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 2, new KeyValuePair<FactType, int>[] { } },
+            };
+            change3.Setup(c => c.GetFactsBefore()).Returns(beforeFacts);
+            afterFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                { 2, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.EnemyPresence, 3)} },
+            };
+            change3.Setup(c => c.GetFactsAfter()).Returns(afterFacts);
+
+
+            // --------------------------
+            // ACT: APPLY ALL THE CHANGES
+            // --------------------------
+
+            WorldRepresentation changed1 = worldUpdator.ApplyDynamicStateChange(originalWorld, change1.Object);
+            WorldRepresentation changed2 = worldUpdator.ApplyDynamicStateChange(changed1, change2.Object);
+            WorldRepresentation changed3 = worldUpdator.ApplyDynamicStateChange(changed2, change3.Object);
+
+            // -----------------------------------------------------------
+            // ASSERT: Final result should match our Default dynamic state
+            // -----------------------------------------------------------
+
+            HardCodedStateCreator.CheckTestStaticState(changed3.StaticState);
+            HardCodedStateCreator.CheckTestDynamicState(changed3.DynamicState);
         }
     }
 }
---
+++ TacticalGameAiUnitTests/DecisionLayer/WorldRepresentationSystem/WorldUpdatorSystem/DynamicStateChangeMocker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using TacticalGameAi.DecisionLayer.WorldRepresentationSystem.WorldUpdatorSystem;
+using TacticalGameAi.DecisionLayer.WorldRepresentationSystem.ValueObjects;
+using TacticalGameAi.DecisionLayer.WorldRepresentationSystem.DynamicStateHiddenTypes;
+using TacticalGameAiUnitTests.DecisionLayer.WorldRepresentationSystem.ValueObjects;
+using Moq;
+using NUnit.Framework;
+using Newtonsoft.Json;
+
+namespace TacticalGameAiUnitTests.DecisionLayer.WorldRepresentationSystem.WorldUpdatorSystem {
+    public static class DynamicStateChangeMocker {
+        public static Mock<IDynamicStateChange> Create(string affectedNodesJson, string beforeFactsJson, string afterFactsJson) {
+            Mock<IDynamicStateChange> change = new Mock<IDynamicStateChange>();
+
+            // Setup affected nodes.
+            var affectedNodes = JsonConvert.DeserializeObject<IEnumerable<int>>(affectedNodesJson);
+            change.Setup(c => c.AffectedNodes).Returns(affectedNodes);
+
+            var beforeFacts = JsonConvert.DeserializeObject<Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>>>(beforeFactsJson);
+            var afterFacts = JsonConvert.DeserializeObject<Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>>>(afterFactsJson);
+            change.Setup(c => c.GetFactsBefore()).Returns(beforeFacts);
+            change.Setup(c => c.GetFactsAfter()).Returns(afterFacts);
+
+            // TODO 6 - MAKE DYNAMICSATECHANGEMOCKER able to parse JSON representing a state change to testing is faster!
+
+            /*
+            Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> beforeFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                // No facts are removed or updated in node 2 by this change.
+                { 2, new KeyValuePair<FactType, int>[] { } },
+
+                // Node 4 has the friendlies fact removed by this change. Thus it must appear in the 'before' set so that this change is reversible.
+                { 4, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.FriendlyPresence, 1)} },
+
+                // No facts are removed or updated in node 5 by this change.
+                { 5, new KeyValuePair<FactType, int>[] { } }
+            };*/
+            /*
+            Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> afterFacts = new Dictionary<int, IEnumerable<KeyValuePair<FactType, int>>> {
+                // Enemies were spotted in node 2 during this event
+                { 2, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.EnemyPresence, 2)} },
+
+                // Node 4 has danger applied to it with a super high rating since friendlies were wiped out!
+                { 4, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.Danger, 7)} },
+
+                // Node 5 also had danger applied to it!
+                { 5, new KeyValuePair<FactType, int>[] { new KeyValuePair<FactType, int>(FactType.Danger, 2)} }
+            };*/
+
+            return change;
+        }
+    }
+}
